d3fef71f395b666fca05e7559e88a377b51d8a9f
VarunIrani <varunirani0505@gmail.com>
2018-11-17 16:45:12 +0530

    Some Few Changesa

--- 14 CopyFile/CopyFile.java
+++ 14 CopyFile/CopyFile.java
@@ -2,24 +2,20 @@ import static java.lang.System.*;
 import java.io.*;
 
 class CopyFile {
-  public static void main(String[] args) throws IOException, FileNotFoundException{
+  public static void main(String[] args) throws IOException, FileNotFoundException {
     File file1 = new File("file1.txt");
     File file2 = new File("file2.txt");
-    String file1Contents = "";
+    Reader r = null;
+    Writer w = null;
 
-    // Read and store contents of file1.txt into String file1Contents
-    FileReader fr = new FileReader(file1);
-    BufferedReader br = new BufferedReader(fr);
-    out.printf("\nReading Data from File 1 (file1.txt)...");
-    file1Contents = br.readLine();
-    out.printf("\nFile 1 contents: \n%s\n", file1Contents);
-    br.close();
-
-    // Write String file1Contents to file2.txt
-    FileWriter fw = new FileWriter(file2);
-    BufferedWriter bw = new BufferedWriter(fw);
-    out.printf("\nWriting Data to File 2 (file2.txt)...");
-    bw.write(file1Contents);
-    bw.close();
+    r = new FileReader(file1);
+    w = new FileWriter(file2);
+    char[] cbuf = new char[1024];
+    int length;
+    while ((length = r.read(cbuf)) > 0) {
+      w.write(cbuf, 0, length);
+    }
+    r.close();
+    w.close();
   }
 }
